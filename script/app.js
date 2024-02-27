105e6a7172d4f5efdf5d77a7a83f4c5f303a7576
Tahfeez Mizan <89704978+tahfeezmizan@users.noreply.github.com>
2024-02-27 22:25:07 +0600

    load data form api

---
+++ script/app.js
@@ -0,0 +1,12 @@
+
+const loadPhone = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    const phones = data.data
+    // console.log(phones)
+    displayPhones(phones)
+}
+
+
+
+loadPhone()
