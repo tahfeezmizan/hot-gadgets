211ed6b6ea8cf56f82756f2e9345ead7b8ab06be
Tahfeez Mizan <89704978+tahfeezmizan@users.noreply.github.com>
2024-02-27 23:18:01 +0600

    add search function

--- script/app.js
+++ script/app.js
@@ -1,6 +1,6 @@
 
-const loadPhone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchItem) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchItem}`);
     const data = await res.json();
     const phones = data.data
     // console.log(phones)
@@ -10,6 +10,7 @@ const loadPhone = async () => {
 const displayPhones = phones => {
 
     const phoneContainer = document.getElementById('item-container');
+    phoneContainer.textContent = '';
     // console.log(phones)
     phones.forEach(phone => {
         console.log(phone)
@@ -19,19 +20,29 @@ const displayPhones = phones => {
         phoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
         phoneCard.innerHTML = `
         <figure class="px-10 pt-10">
-            <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
+            <img src="${phone.image}" alt="Shoes" class="rounded-xl" />
         </figure>
         
         <div class="card-body items-center text-center">
-            <h2 class="card-title">Shoes!</h2>
+            <h2 class="card-title text-3xl">${phone. phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
             <div class="card-actions">
-            <button class="btn btn-primary">Buy Now</button>
+            <button class="btn btn-primary">Show Details</button>
             </div>
         </div>
         `;
         phoneContainer.appendChild(phoneCard);
     });
+}
+
+
+// handle search button 
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText)
+    loadPhone(searchText)
+    searchText.value = '';
 }
 
 loadPhone()
