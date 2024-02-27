7cb94b14d95a97d6601bb968c74e378e2b95c848
Tahfeez Mizan <89704978+tahfeezmizan@users.noreply.github.com>
2024-02-27 22:49:06 +0600

    data display

--- script/app.js
+++ script/app.js
@@ -7,6 +7,31 @@ const loadPhone = async () => {
     displayPhones(phones)
 }
 
+const displayPhones = phones => {
 
+    const phoneContainer = document.getElementById('item-container');
+    // console.log(phones)
+    phones.forEach(phone => {
+        console.log(phone)
+
+        // create a div 
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card w-96 bg-base-100 shadow-xl`;
+        phoneCard.innerHTML = `
+        <figure class="px-10 pt-10">
+            <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
+        </figure>
+        
+        <div class="card-body items-center text-center">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions">
+            <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+        `;
+        phoneContainer.appendChild(phoneCard);
+    });
+}
 
 loadPhone()
